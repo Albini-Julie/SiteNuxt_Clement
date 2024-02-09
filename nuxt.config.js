04bbc4a5362b87cb8cf0d014f0460cdbad859b1f
Julie <julie.albini@edu.univ-fcomte.fr>
2024-02-09 10:40:17 +0100

    feat(scss) : installation de scss et tests réussis

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  components: true,
+  css: ['@/scss/main.scss'],
   modules: ['@nuxtjs/prismic'],
   prismic: { endpoint: 'https://rally.cdn.prismic.io/api/v2' },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/scss/foundations/_variables.scss";
+          @import "@/scss/foundations/_mixins.scss";`
+        }
+      }
+    }
+  },
 })
