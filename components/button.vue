337d407a0e506134e6fecab3bb6c18ecc8f86cb0
Julie <julie.albini@edu.univ-fcomte.fr>
2024-02-12 19:32:22 +0100

    feat(routage) : intégration responsive de la partie routage

--- components/button.vue
+++ components/button.vue
@@ -1,9 +1,12 @@
 <template>
-  <a class="cover__lien" :href="lien" download>
-    <div class="button">
-      <p class="button__text">{{ text }}</p>
-    </div>
-  </a>
+  <NuxtLink class="button__lien" :to="route">
+    <a class="button__lien" :href="lien" download>
+      <div class="button">
+        <p class="button__text">{{ text }}</p>
+        <IconsFleche v-if="fleche" />
+      </div>
+    </a>
+  </NuxtLink>
 </template>
 
 <style lang="scss" scoped>
@@ -11,11 +14,33 @@
   background-color: v-bind(color);
   padding: rem(10) rem(30);
   width: fit-content;
+  display: flex;
+  align-items: center;
+  gap: rem(20);
+
+  &:hover {
+    animation: pulse 1s;
+    box-shadow: 0 0 0 2em transparent;
+
+    @keyframes pulse {
+      0% {
+        box-shadow: 0 0 0 0 v-bind(color);
+      }
+    }
+  }
 
   &__text {
     color: v-bind(colorText);
     font-family: $primary-font-family;
     font-size: rem(18);
+    @include x-large-up {
+      font-size: rem(20);
+    }
+  }
+
+  &__lien {
+    text-decoration: none;
+    text-transform: none;
   }
 }
 </style>
@@ -25,6 +50,8 @@ defineProps({
   color: String,
   colorText: String,
   text: String,
+  route: String,
+  fleche: Boolean,
   lien: String,
 });
 </script>
