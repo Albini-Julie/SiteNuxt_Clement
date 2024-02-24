f867ee5e87bd76acad9629b9967ae3feca9590a9
Julie <julie.albini@edu.univ-fcomte.fr>
2024-02-24 17:44:35 +0100

    feat(pilote) : intégration responsive de la partie projet

---
+++ components/icons/triangle.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg
+    width="15"
+    height="18"
+    viewBox="0 0 15 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="none"
+  >
+    <path d="M15 9L0 17.6603L0 0.339746L15 9Z" fill="white"></path>
+  </svg>
+</template>
